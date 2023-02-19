ed873498bb42179fc93636318621f0c206284daa
Khryzen <mdeguzman.cpe@gmail.com>
2023-02-19 16:23:39 +0800

    Initial commit

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 func DBSettings() {
 	uadmin.Database = &uadmin.DBSettings{
-		Type:     "myslq",
+		Type:     "mysql",
 		Name:     "shop",
 		User:     "root",
 		Password: "Allen is Great 200%",
